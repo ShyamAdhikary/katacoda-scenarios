66d2c43fa1b7b563ab23e0bf91ee7c098a868eb6
ShyamAdhikary <58951101+ShyamAdhikary@users.noreply.github.com>
2021-01-17 23:14:35 +0530

    Add files via upload

---
+++ Kat-scn-1.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+#
+#Use this to Test small code snippets by inserting the new code
+#
+print ("Testing starts-1:", datetime.today().strftime("%x %H:%M:%S"))
+
+# 
+
