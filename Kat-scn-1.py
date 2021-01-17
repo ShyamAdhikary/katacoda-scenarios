8c5dc6f1354593d65a37205e2edabd8dedac5436
ShyamAdhikary <58951101+ShyamAdhikary@users.noreply.github.com>
2021-01-17 23:53:27 +0530

    Update Kat-scn-1.py

--- Kat-scn-1.py
+++ Kat-scn-1.py
@@ -4,7 +4,8 @@
 #
 #Use this to Test small code snippets by inserting the new code
 #
-print ("Testing starts-1:", datetime.today().strftime("%x %H:%M:%S"))
+print ("Testing starts-1:")
+print ("Testing Katacoda-1")
 
 # 
 
